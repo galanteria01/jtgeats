ed7bf9884e27846c21fc3a6f2cc3d1bdbf334afc
Shoury Sharma <shanuu12e@gmail.com>
2022-09-17 14:55:35 +0530

    chore: Add minor responsive website aspects

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,29 @@
 let cartButton = document.querySelector('#nav--cartopener');
 let cartClose = document.querySelector('#cart--close');
-
 let cartModal = document.querySelector('.cart');
+
+let requestButton = document.querySelector('#items--requestopener');
+let requestClose = document.querySelector('.request--buttonsleft');
+let requestModal = document.querySelector('.request');
 
 cartButton.onclick = function() {
   cartModal.style.display = 'block';
+  document.body.classList.add('lock-scroll');
 }
 
 cartClose.onclick = function() {
   cartModal.style.display = 'none';
+  document.body.classList.remove('lock-scroll');
+}
+
+requestButton.onclick = function() {
+  requestModal.style.display = 'block';
+  document.body.classList.add('lock-scroll');
+}
+
+requestClose.onclick = function() {
+  requestModal.style.display = 'none';
+  document.body.classList.remove('lock-scroll');
 }
 
 // Event to disable modal as soon as click is observed
