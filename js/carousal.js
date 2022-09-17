6a397f60fbdf175f03dd3aa391e88df0db75caed
Shoury Sharma <shanuu12e@gmail.com>
2022-09-17 15:56:23 +0530

    chore: Initial implementation finished

--- js/carousal.js
+++ js/carousal.js
@@ -1,10 +1,19 @@
-
-
-$(document).ready(function(){
-  $('.items--carousal').slick({
-  slidesToShow: 3,
-  dots:true,
-  centerMode: true,
-  });
+$(document).ready(function () {
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    $('.items--carousal').slick({
+      // slidesToShow: 1,
+      centerMode: true,
+    });
+  } else if (window.matchMedia("(max-width: 992px)").matches) {
+    $('.items--carousal').slick({
+      slidesToShow: 2,
+      centerMode: true,
+    });
+  } else {
+    $('.items--carousal').slick({
+      slidesToShow: 3,
+      centerMode: true,
+    });
+  }
 });
 
--- js/items.js
+++ js/items.js
@@ -5,7 +5,7 @@ let count = 16;
 const listOfFoodItems = [
   {
     id: 1,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img1.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
@@ -14,106 +14,105 @@ const listOfFoodItems = [
   },
   {
     id: 2,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img2.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
+    time: "50-79 min",
     discount: ""
   },
   {
     id: 1,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img3.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
+    time: "50-79 min",
     discount: ""
   },
   {
     id: 4,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img4.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
+    time: "50-79 min",
     discount: ""
   },
   {
     id: 5,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img5.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
-    discount: ""
+    time: "50-79 min",
+    discount: "20%"
   },
   {
     id: 6,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img4.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
+    time: "50-79 min",
     discount: ""
   },
   {
     id: 7,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img3.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
+    time: "50-79 min",
     discount: ""
   },
   {
     id: 8,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img2.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
+    time: "50-79 min",
     discount: ""
   },
   {
     id: 9,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img5.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
-    discount: ""
+    time: "50-79 min",
   },
   {
     id: 10,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img1.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
+    time: "50-79 min",
     discount: ""
   },
   {
     id: 11,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img3.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
+    time: "50-79 min",
     discount: ""
   },
   {
     id: 12,
-    image: 'https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000',
+    image: './assets/foods/img5.png',
     title: "Home made pizza",
     price: "₹160",
     rating: "4.7",
-    time: "",
+    time: "50-79 min",
     discount: ""
   },
 ]
 
-listOfFoodItems.forEach((item) => {
+function generateCard(item, badges) {
   // Declaring the elements
   let element = document.createElement('div');
   let img = document.createElement('img');
@@ -128,30 +127,36 @@ listOfFoodItems.forEach((item) => {
   let rowBottomRight = document.createElement('div');
   let badge = document.createElement('p');
   let icon = document.createElement('img')
-  let star = document.createElement('img')
+  let star = document.createElement('img');
+  let timecontainer = document.createElement('div')
+  let ratingValue = document.createElement('p');
 
   // Providing the connection between the elements
   element.appendChild(img);
   element.appendChild(rowTop);
   element.appendChild(rowBottom);
-  if(item.discount) element.appendChild(badge);
+  if(item.discount && badges) element.appendChild(badge);
   rowTop.appendChild(title);
   rowTop.appendChild(price);
   rowBottom.appendChild(rowBottomLeft);
   rowBottom.appendChild(rowBottomRight);
   rowBottomLeft.appendChild(rating);
-  rowBottomLeft.appendChild(time);
+  rowBottomLeft.appendChild(timecontainer);
   rowBottomRight.appendChild(add);
   add.appendChild(icon);
+  rating.appendChild(star);
+  rating.appendChild(ratingValue);
+  timecontainer.appendChild(time)
 
   // Assigning the values based on object data
   title.innerHTML = item.title;
   price.innerHTML = item.price;
   img.src = item.image;
-  rating.innerHTML = item.rating;
+  ratingValue.innerHTML = item.rating;
   time.innerHTML = item.time;
   badge.innerHTML = item.discount
   icon.src = "./assets/icons/add.svg";
+  star.src = "./assets/icons/star.svg"
 
   // Assigning the specific classes for the elements
   element.classList.add('items--card')
@@ -162,60 +167,21 @@ listOfFoodItems.forEach((item) => {
   rowBottomRight.classList.add('items--rowbottomright');
   add.classList.add('items--addbutton')
   badge.classList.add('items--badge')
+  time.classList.add('items--time')
+  timecontainer.classList.add('items--timecontainer');
+  rating.classList.add('items--rating');
+  star.classList.add('items--star')
 
+  return element;
+}
+
+listOfFoodItems.forEach((item) => {
+  let element = generateCard(item, false);
   carousal.appendChild(element);
 })
 
 listOfFoodItems.forEach((item) => {
-  // Declaring the elements
-  let element = document.createElement('div');
-  let img = document.createElement('img');
-  let title = document.createElement('p');
-  let rating = document.createElement('div');
-  let time = document.createElement('p');
-  let price = document.createElement('p');
-  let add = document.createElement('button');
-  let rowTop = document.createElement('div');
-  let rowBottom = document.createElement('div');
-  let rowBottomLeft = document.createElement('div');
-  let rowBottomRight = document.createElement('div');
-  let badge = document.createElement('p');
-  let icon = document.createElement('img')
-  let star = document.createElement('img')
-
-  // Providing the connection between the elements
-  element.appendChild(img);
-  element.appendChild(rowTop);
-  element.appendChild(rowBottom);
-  if(item.discount) element.appendChild(badge);
-  rowTop.appendChild(title);
-  rowTop.appendChild(price);
-  rowBottom.appendChild(rowBottomLeft);
-  rowBottom.appendChild(rowBottomRight);
-  rowBottomLeft.appendChild(rating);
-  rowBottomLeft.appendChild(time);
-  rowBottomRight.appendChild(add);
-  add.appendChild(icon);
-
-  // Assigning the values based on object data
-  title.innerHTML = item.title;
-  price.innerHTML = item.price;
-  img.src = item.image;
-  rating.innerHTML = item.rating;
-  time.innerHTML = item.time;
-  badge.innerHTML = item.discount
-  icon.src = "./assets/icons/add.svg";
-
-  // Assigning the specific classes for the elements
-  element.classList.add('items--card')
-  img.classList.add('items--image')
-  rowTop.classList.add('items--rowtop');
-  rowBottom.classList.add('items--rowbottom')
-  rowBottomLeft.classList.add('items--rowbottomleft');
-  rowBottomRight.classList.add('items--rowbottomright');
-  add.classList.add('items--addbutton')
-  badge.classList.add('items--badge')
-
+  let element = generateCard(item, true);
   list.appendChild(element);
 })
 
